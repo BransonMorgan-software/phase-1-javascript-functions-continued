dfc829ad43f082ab0e6fa270c488a0d8643789bc
Branson Morgan <bransox@comcast.net>
2022-08-10 10:19:21 -0400

    commit tree ae180957747e49e122fc6a4a1c645d98e78ea35a

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function mondayWork (activity = "go to the office") {
     return (`This Monday, I will ${activity}.`);
 }
 mondayWork();
+
+function wrapAdjective (star = "*") {
+    return function (you = "special") {
+        return `You are ${star}${you}${star}!`;
+    }
+}
+wrapAdjective(star)(you);
+    
